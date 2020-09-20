c6163fe7ce9f9b434f088e7546549a3a694cb7eb
Nathan Glick <nateleighglick@hotmail.com>
2020-09-20 10:53:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function sayHiToGrandma(string){
   if(string.toLowerCase())
   return "I can't hear you!"
   else if(string.toUpperCase())
-  return "YES INDEED!"
+  return "I can't hear you" === "YES INDEED!"
   else if(string.toLowerCase("I love you, Grandma."))
   return "I love you too."
 }
