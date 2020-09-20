9a569238c5ea1de35cf4cafe70f0ca6612143ae2
Nathan Glick <nateleighglick@hotmail.com>
2020-09-20 10:35:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,8 +14,7 @@ function logWhisper(string){
   console.log(string.toLowerCase())
 
 function sayHiToGrandma(string){
-  if (string.toLowerCase())
-  return "I can't hear you"
+  return string.toLowerCase("I can't hear you!")
 }
 
 }
